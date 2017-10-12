e89cc4072ee7bf43776ce8d40cf7e9cb5261cb20
ConorM16 <muganc@tcd.ie>
2017-10-12 23:29:33 +0100

    Initial working program

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,53 +1,48 @@
-//import BinaryTree.Node;
+ //import BinaryTree.Node;
 
 public class BinaryTree {
-	 private Node root;             // root of BST
-
+	 public Node root;             // root of BST
+//	 private boolean isEmpty = true;
 	    /**
 	     * Private node class.
 	     */
 	    private class Node {
-	        private Node left, right;  // left and right subtrees
+	        private Node left,right,parent;  // left and right subtrees
 	        private int N;             // number of nodes in subtree
 
 	        public Node(int N) {
 	            this.N = N;
+	            left = null;
+	            right = null;
+	            parent = null;
 	        }
 	    }
 	    
-	    public static void createTree(int [] array)
-	    {
-	    	int mid;
-	    	BinaryTree tree = new BinaryTree();
-	    	mid = findRoot(array);
-	    	putRoot(array[mid], tree);
-	    	
-	    	
+	    public void putNode(int n){
+	    	root = put(root, n);
 	    }
 	    
-	    public static int findRoot(int [] array)
-	    {
-	    	int mid = 0;
-	    	if(array.length == 0 || array.length == 1)
-	    	{
-	    		return 0;
-	    	}
-	    	else
-	    	{
-	    		mid = ((array.length)/2)-1;
-	    		return mid;
-	    	}
+	    private Node put(Node x, int n) {
+	        if (x == null) return new Node(n);
+	        if      (n < x.N) x.left  = put(x.left, n);
+	        else if (n > x.N) x.right = put(x.right, n);
+	        else              x.N   = n;
+	        return x;
 	    }
 	    
-	    public static void putRoot(int N, BinaryTree tree)
+	    public boolean contains(int n)
 	    {
-	    	Node node = new Node(N);
-	    	tree.root = node;  
+	    	return containsNode(root, n);
 	    }
 	    
-	    public void add(BinaryTree tree, int val, Node N)
+	    public boolean containsNode(Node x,int n)
 	    {
+	    	if(x == null) return false;
 	    	
+	    	if		(n < x.N) return containsNode(x.left,n);
+	    	else if (n > x.N) return containsNode(x.right,n);
+	    	else return true;
 	    }
+	
 
 }
